0893669bff818e702eeef2cb0c920d75cef8f771
Ahmad Kaif <heyahmad611@gmail.com>
2025-04-09 00:18:37 +0530

    Flask baisc server setup

---
+++ README.md
@@ -0,0 +1,2 @@
+## Flask
+**WebFramework for python**
---
+++ basicApp/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "hello, Flask, This is the home page"
+
+@app.route('/about')
+def about():
+    return "This is the about page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
